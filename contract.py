a6dff385025e8a9c8c8d9cf143e38d1aa4322051
Paul J Stevens <paul@nfg.nl>
2010-10-14 12:34:19 +0000

    wip workflow and triggers

--- contract.py
+++ contract.py
@@ -1,19 +1,22 @@
 
 from __future__ import with_statement
 from decimal import Decimal
-from trytond.model import ModelView, ModelSQL, fields
+from trytond.model import ModelView, ModelSQL, ModelWorkflow, fields
 from trytond.wizard import Wizard
 from trytond.report import Report
 from trytond.tools import reduce_ids
 from trytond.pyson import Equal, Eval, Not, PYSONEncoder, Date, Bool
 from trytond.transaction import Transaction
 
+import logging
+
+log = logging.getLogger(__name__)
 
 STATES = {
-    'readonly':  Bool(Eval('active')),
+    'readonly': Not(Equal(Eval('state'), 'draft')),
 }
 
-class Contract(ModelSQL, ModelView):
+class Contract(ModelWorkflow, ModelSQL, ModelView):
     """Contract Agreement"""
     _name = 'contract.contract'
     _description = __doc__
@@ -23,21 +26,50 @@ class Contract(ModelSQL, ModelView):
                             states=STATES)
     product = fields.Many2One('product.product', 'Product', required=True,
                               states=STATES)
-
-    active = fields.Boolean('Active', select=1)
+    state = fields.Selection([
+        ('draft','Draft'),
+        ('active','Active'),
+        ('canceled','Canceled'),
+    ], 'State', readonly=True)
     interval = fields.Selection([
         ('day','Day'),
         ('week','Week'),
         ('month','Month'),
         ('year','Year'),
     ], 'Interval', required=True, states=STATES)
-    interval_quant = fields.Property(fields.Numeric('Interval count',
-                                                    digits=(16,2)))
-    next_invoice_date = fields.Date('Next Invoice')
-    start_date = fields.Date('Since')
+    interval_quant = fields.Numeric('Interval count', digits=(16,2),
+                                    states=STATES)
+    next_invoice_date = fields.Date('Next Invoice', states=STATES)
+    start_date = fields.Date('Since', states=STATES)
     stop_date = fields.Date('Until')
+    lines = fields.One2Many('account.invoice.line', 'contract', 'Invoice Lines')
 
+    def default_state(self):
+        return 'draft'
+
+    def default_interval(self):
+        return 'month'
+
+    def default_interval_quant(self):
+        return Decimal("3.0")
+
+    def write(self, ids, vals):
+        log.info("%s:%s" % ( ids, vals))
+        super(Contract, self).write(ids, vals)
+        if 'state' in vals:
+            self.workflow_trigger_trigger(ids)
+
+        return None
 
 Contract()
+
+
+class InvoiceLine(ModelSQL, ModelView):
+    """Invoice Line"""
+    _name = 'account.invoice.line'
+    contract = fields.Many2One('contract.contract', 'Contract')
+
+
+InvoiceLine()
 
 
