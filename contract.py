37a8479a279978db929dc0a2b85487959cd41e91
Paul J Stevens <paul@nfg.nl>
2010-10-15 11:21:42 +0000

    finish invoicing wizard

--- contract.py
+++ contract.py
@@ -5,9 +5,11 @@ from trytond.model import ModelView, ModelSQL, ModelWorkflow, fields
 from trytond.wizard import Wizard
 from trytond.report import Report
 from trytond.tools import reduce_ids
-from trytond.pyson import Equal, Eval, Not, PYSONEncoder, Date, Bool
+from trytond.pyson import Equal, Eval, Not, PYSONEncoder, Date, Bool, If, In, Get
 from trytond.transaction import Transaction
 
+import datetime
+import time
 import logging
 
 log = logging.getLogger(__name__)
@@ -21,7 +23,19 @@ class Contract(ModelWorkflow, ModelSQL, ModelView):
     _name = 'contract.contract'
     _description = __doc__
 
+    company = fields.Many2One('company.company', 'Company', required=True,
+                         states=STATES, select=1, domain=[
+                             ('id', If(In('company', Eval('context', {})), '=', '!='),
+                              Get(Eval('context', {}), 'company', 0)),
+                         ])
+
+    journal = fields.Many2One('account.journal', 'Journal', required=True,                                                                                                             
+                                      states=STATES, domain=[('centralised', '=', False)])
+
     name = fields.Char('Name', required=True, translate=True)
+
+    description = fields.Char('Description', size=None, translate=True)
+
     party = fields.Many2One('party.party', 'Party', required=True, 
                             states=STATES)
     product = fields.Many2One('product.product', 'Product', required=True,
@@ -51,7 +65,7 @@ class Contract(ModelWorkflow, ModelSQL, ModelView):
     start_date = fields.Date('Since', states=STATES)
     stop_date = fields.Date('Until')
     lines = fields.One2Many('account.invoice.line', 'contract', 'Invoice Lines',
-                           readonly=True)
+                           readonly=True, domain=[('contract','=',Eval('id'))])
 
     def default_state(self):
         return 'draft'
@@ -80,7 +94,6 @@ class InvoiceLine(ModelSQL, ModelView):
     _name = 'account.invoice.line'
     contract = fields.Many2One('contract.contract', 'Contract')
 
-
 InvoiceLine()
 
 class CreateNextInvoice(Wizard):
@@ -97,16 +110,87 @@ class CreateNextInvoice(Wizard):
     }
 
     def _next_invoice(self, data):
-        log.debug('data: %s' % data)
-
         id = data.get('id')
         contract_obj = self.pool.get('contract.contract')
         contract = contract_obj.browse([id])[0]
+        if not contract.state == 'active':
+            return {}
 
-        log.debug('contract: %s' % contract)
-        log.debug('party: %s' % contract.party)
-        log.debug('product: %s' % contract.product)
+        invoice_obj = self.pool.get('account.invoice')
+        line_obj = self.pool.get('account.invoice.line')
 
+        invoice_addr = contract.party.addresses[0]
+        for addr in contract.party.addresses:
+            if addr.invoice == True:
+                invoice_addr = addr
+                break
+
+        today = datetime.date.fromtimestamp(time.time())
+
+        last_date = contract.next_invoice_date or contract.start_date or today
+
+        if contract.interval == 'year':
+            next_date = datetime.date(last_date.year + contract.interval_quant,
+                                      last_date.month, 
+                                      last_date.day)
+        elif contract.interval == 'month':
+            next_year = last_date.year + (last_date.month + int(contract.interval_quant)) / 12
+            next_month = (last_date.month + int(contract.interval_quant)) % 12 
+            next_date = datetime.date(next_year, next_month, last_date.day)
+
+        elif contract.interval == 'week':
+            delta = datetime.timedelta(0,0,0,0,0,0,contract.interval_quant)
+            next_date = last_date + delta
+
+        elif contract.interval == 'day':
+            next_date = last_date + datetime.timedelta(contract.interval_quant)
+
+        if contract.next_invoice_date and today + datetime.timedelta(30) < contract.next_invoice_date:
+            log.info('too early to invoice: %s + 30 days < %s', today, next_date)
+            return {}
+
+        ## create a new invoice
+        invoice = invoice_obj.create(dict(
+            company=contract.company.id,
+            type='out_invoice',
+            reference=contract.product.code or contract.product.name,
+            description=contract.description or contract.name,
+            state='draft',
+            currency=contract.company.currency.id,
+            journal=contract.journal.id,
+            account=contract.party.account_receivable.id,
+            payment_term=contract.party.payment_term.id,
+            party=contract.party.id,
+            invoice_address=invoice_addr.id
+        ))
+
+        ## create invoice line
+        account = contract.account or \
+                contract.product.account_revenue or \
+                contract.product.category.account_revenue
+
+        line = line_obj.create(dict(
+            type='line',
+            product=contract.product.id,
+            invoice=invoice,
+            description="%s (%s) %s - %s" % (contract.product.description or
+                                             contract.product.name,
+                                             contract.name,
+                                             last_date, next_date),
+            quantity=contract.interval_quant,
+            account=account.id,
+            unit=contract.product.default_uom.id,
+            unit_price=contract.product.list_price,
+            contract=contract.id,
+        ))
+
+        ## open invoice
+        invoice = invoice_obj.browse([invoice])[0]
+        invoice.write(invoice.id, {'invoice_date': today})
+        invoice.workflow_trigger_validate(invoice.id, 'open')
+
+        ## update fields
+        contract.write(contract.id, {'next_invoice_date': next_date})
 
         return {}
 
